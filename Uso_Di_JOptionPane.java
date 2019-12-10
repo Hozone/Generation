2dfb2568a222daacbf5ae09a5587328dbefa2037
Giuseppe <giuseppe.fahmy@gmail.com>
2019-12-10 17:28:13 +0100

    Add files via upload

---
+++ Uso_Di_JOptionPane.java
@@ -0,0 +1,11 @@
+
+import javax.swing.*;
+class Uso_Di_JOptionPane 
+{
+	public static void main(String[] args) 
+	{
+
+		String h=JOptionPane.showInputDialog("inserisci");
+		System.out.println(h);
+	}
+}
