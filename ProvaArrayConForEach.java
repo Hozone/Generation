977cdccfa2e83562a4730de4943885f4489d1bf3
Giuseppe <giuseppe.fahmy@gmail.com>
2019-12-12 11:04:02 +0100

    Add files via upload

---
+++ ProvaArrayConForEach.java
@@ -0,0 +1,14 @@
+class ProvaArrayConForEach 
+{
+	public static void main(String[] args) 
+	{
+
+		String[] nomi={"mario","francesco","giuseppe"};
+		
+		for( String item:nomi  ){
+
+			System.out.println(item);
+		}
+		
+	}
+}
